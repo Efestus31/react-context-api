0bb3bb8215ea969183e48815b8a967cab856f12e
Efestus31 <biagiohd@gmail.com>
2024-11-27 13:37:17 +0100

    add post card component

---
+++ src/components/PostCard.jsx
@@ -0,0 +1,57 @@
+import { useState, useEffect, useContext } from 'react'
+import { Link } from 'react-router-dom';
+import GlobalContext from '../contexts/GlobalContext'
+
+
+export default function PostCard() {
+    const [postsData, setPostsData] = useState({})
+    const { Url_api } = useContext(GlobalContext)
+
+    function fetchData(url = `${Url_api}/posts`) {
+        fetch(url)
+            .then(resp => resp.json())
+            .then(data => {
+                console.log(data);
+                setPostsData(data)
+
+            })
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+
+
+
+
+
+
+
+    return (
+        <>
+            {
+                postsData.data ?
+                    postsData.data.map((post, index) => (
+                        <div className="col" key={post.id || index}>
+                            <div className="card">
+                                <h3>
+                                    {post.title}
+                                </h3>
+                                <Link to={`/posts/${post.slug}`}>
+                                    <img src={`${Url_api}/` + post.image} alt={post.title} style={{ width: '100%', height: '10rem', aspectRatio: '1' }} />
+                                </Link>
+
+                            </div>
+                            <div className='border border-gray-400 p-2'>
+
+                                {post.content}
+                            </div>
+
+                        </div>
+                    )) :
+                    <p>No data found</p>
+            }
+        </>
+    )
+}
--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,30 +1,10 @@
-import { useState, useEffect, useContext } from 'react'
-import { Link } from 'react-router-dom';
-import GlobalContext from '../contexts/GlobalContext'
+
+import PostCard from '../components/PostCard';
 
 
 
 
 export default function Posts() {
-    const [postsData, setPostsData] = useState({})
-    const { Url_api } = useContext(GlobalContext)
-
-    function fetchData(url = `${Url_api}/posts`) {
-        fetch(url)
-            .then(resp => resp.json())
-            .then(data => {
-                console.log(data);
-                setPostsData(data)
-
-            })
-    }
-
-    useEffect(() => {
-        fetchData()
-    }, [])
-
-
-
     return (
         <>
             <h1>
@@ -33,28 +13,7 @@ export default function Posts() {
             <section className='posts'>
                 <div className="container">
                     <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3">
-                        {
-                            postsData.data ?
-                                postsData.data.map((post, index) => (
-                                    <div className="col" key={post.id || index}>
-                                        <div className="card">
-                                            <h3>
-                                                {post.title}
-                                            </h3>
-                                            <Link to={`/posts/${post.slug}`}>
-                                                <img src={`${Url_api}/` + post.image} alt={post.title} style={{ width: '100%', height: '10rem', aspectRatio: '1' }} />
-                                            </Link>
-
-                                        </div>
-                                        <div className='border border-gray-400 p-2'>
-
-                                            {post.content}
-                                        </div>
-
-                                    </div>
-                                )) :
-                                <p>No data found</p>
-                        }
+                        <PostCard />
                     </div>
                 </div>
             </section>
